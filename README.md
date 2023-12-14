6598bf15243b66de6281f4b2f943e4bdb3254005
John Baker <jcbake707@gmail.com>
2023-12-14 08:55:03 -0600

    Added uart.c & .h files with rx isr functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+# UART Driver with ISR
+
+## ISR for Tx functionality onlu
+
+This driver uses interrupt to transmit data
---
+++ uart_isr.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include "uart.h"
+
+#define GPIOA   (1U<<0)
+#define UART2EN (1U<<17)
+
+#define UART2EN (1U<<17)
+#define UART2EN (1U<<17)
+#define UART2EN (1U<<17)
+#define UART2EN (1U<<17)
